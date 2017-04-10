e5e6b9ea82cc0a24f561e396ec4a8718d2dd49be
edycastro1 <edy.castro@outlook.com>
2017-04-10 02:37:00 -0300

    mobile adjustments

--- wp-content/themes/instituto/page-participe.php
+++ wp-content/themes/instituto/page-participe.php
@@ -39,7 +39,7 @@
 		<div class="container">
 			<h3 class="general-title">buscamos embaixadores</h3>
 			<div class="row">
-				<div class="col-sm-8 col-sm-offset-1">
+				<div class="col-sm-8 col-sm-offset-1 seja-embaixador">
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent accumsan mattis ipsum, imperdiet auctor nisl egestas vel. Morbi id libero eu quam venenatis condimentum.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent accumsan mattis ipsum, imperdiet auctor nisl egestas vel. Morbi id libero eu quam venenatis condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent accumsan mattis ipsum, imperdiet auctor nisl egestas vel. Morbi id libero eu quam venenatis condimentum.</p>
 				</div>
 				<a href="javascript:void(0);"><span>baixar</span><span>Kit de imagens</span><span>INAC</span></a>
@@ -127,7 +127,7 @@
 	<section class="voluntarios">
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-8 col-xs-offset-2">
+				<div class="col-sm-8 col-xs-offset-2 seja-voluntario">
 					<h3 class="general-title">precisamos de voluntários</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent accumsan mattis ipsum, imperdiet auctor nisl egestas vel. Morbi id libero eu quam venenatis condimentum.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
 					<button>quero ser um</button>
